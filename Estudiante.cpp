041c5b8134687953751f2f3909645efa5653aa3a
acidozafiro <deiraluca@gmail.com>
2025-08-23 16:16:34 -0300

    Update Estudiante.cpp

--- Estudiante.cpp
+++ Estudiante.cpp
@@ -10,8 +10,20 @@ void Estudiante::setup()
   m_crisis = false;
 }
 
+void Estudiante::update()
+{
+  if (m_tiempo <= 0)
+	{
+    m_crisis = true;
+    m_vidas--;
+    m_tiempo = 100;
+  }
+}
+
 void Estudiante::draw()
 {
+  if (!m_crisis)
+  {
   /******************\ 
    Dibujo del estudiante
            O
@@ -26,12 +38,53 @@ void Estudiante::draw()
   mvaddch(m_y + 2, m_x + 1, '/');
   mvaddch(m_y + 2, m_x + 2, ' ');
   mvaddch(m_y + 2, m_x + 3, '\');
+  }
+  else
+  {
+    // Fotograma 1 de la explosión.
+    /******************\ 
+             *
+            *.*
+            * *
+    \******************/
+    mvprintw(m_y, m_x - 2, "    *    ");
+    mvprintw(m_y + 1, m_x - 2, "   *.*   ");
+    mvprintw(m_y + 2, m_x - 2, "   * *   ");    
+    
+    refresh();
+    delay_output(200);
+
+    // Fotograma 2 de la explosión.
+    /******************\ 
+        \    *    /
+         *  . .  *
+        *  .   .  *
+         /   *   \
+    \******************/
+    mvprintw(m_y - 1, m_x - 3, "\    *    /");
+    mvprintw(m_y, m_x - 2, "*  * *  *");
+    mvprintw(m_y + 1, m_x - 3, "*  *   *  *");
+    mvprintw(m_y + 2, m_x - 2, "*  * *  *");
+    mvprintw(m_y + 3, m_x - 3, " /   *   \ ");
+
+    // Pausa para ver el fotograma 2.
+    refresh();
+    delay_output(200);
+    
+    m_crisis = false;
+  }
 }
 
-///////////////////////////////////////////////////////////////////////////
+//Setters y Getters de posicion
 void Estudiante::setX(int x) { m_x = x; }
 void Estudiante::setY(int y) { m_y = y; }
 
-///////////////////////////////////////////////////////////////////////////
-int Nave::getX() { return m_x; }
-int Nave::getY() { return m_y; }
+int Estudiante::getX() { return m_x; }
+int Estudiante::getY() { return m_y; }
+
+//Setters y Getters de 
+void Estudiante::setTiempo (float tiempo) { m_tiempo = tiempo; }
+int Estudiante::getTiempo() { return m_tiempo; }
+
+void Estudiante::setVidas(int vidas) { m_vidas = vidas; }
+int Estudiante::getVidas() { return m_vidas; }
