3c226af0f86409027ad5ff6718d1071f9451aa99
acidozafiro <deiraluca@gmail.com>
2025-09-12 21:42:14 -0300

    Update Manifestante.cpp

--- Manifestante.cpp
+++ Manifestante.cpp
@@ -38,9 +38,76 @@ void Manifestante::colision(Estudiante &rEstudiante)
   // Verificamos la colisión. box de manifestante es de 9x ; 3y. box de estudiante es de 3x ; 3y
   if(m_x >= rEstudiante.getX() && m_x <= rEstudiante.getX() + 9 && m_y >= rEstudiante.getY() && m_y <= rEstudiante.getY() + 3)
   {
+    //MENSAJES QUE APARECEN DE FORMA RANDOM. ACÁ PODRIAMOS USAR LOS ENUMERADOS Y EL ARRAY?
+    
+    	for (int y = 10; y < 16; y++) mvhline(y, 40, ' ', 40);
+
+    	mvaddch(9, 39, ACS_ULCORNER);
+	    mvaddch(9, 80, ACS_URCORNER);
+	    mvaddch(16, 39, ACS_LLCORNER);
+    	mvaddch(16, 80, ACS_LRCORNER);
+    	mvhline(9, 40, ACS_HLINE, 40);
+	    mvhline(16, 40, ACS_HLINE, 40);
+	    mvvline(10, 39, ACS_VLINE, 6);
+    	mvvline(10, 80, ACS_VLINE, 6);
+
+	    mvprintw(12, 55, "NO AL VETO DE LA LEY DE FINANCIAMIENTO UNIVERSITARIO");
+
+      for (int y = 10; y < 16; y++) mvhline(y, 40, ' ', 40);
+
+    	mvaddch(9, 39, ACS_ULCORNER);
+	    mvaddch(9, 80, ACS_URCORNER);
+	    mvaddch(16, 39, ACS_LLCORNER);
+    	mvaddch(16, 80, ACS_LRCORNER);
+    	mvhline(9, 40, ACS_HLINE, 40);
+	    mvhline(16, 40, ACS_HLINE, 40);
+	    mvvline(10, 39, ACS_VLINE, 6);
+    	mvvline(10, 80, ACS_VLINE, 6);
+
+	    mvprintw(12, 55, "RECOMPOSICIÓN SALARIAL DOCENTE YA");
+
+      for (int y = 10; y < 16; y++) mvhline(y, 40, ' ', 40);
+
+    	mvaddch(9, 39, ACS_ULCORNER);
+	    mvaddch(9, 80, ACS_URCORNER);
+	    mvaddch(16, 39, ACS_LLCORNER);
+    	mvaddch(16, 80, ACS_LRCORNER);
+    	mvhline(9, 40, ACS_HLINE, 40);
+	    mvhline(16, 40, ACS_HLINE, 40);
+	    mvvline(10, 39, ACS_VLINE, 6);
+    	mvvline(10, 80, ACS_VLINE, 6);
+
+	    mvprintw(12, 55, "ESTUDIÁ, NO SEAS LIBERTARIO");
+
+      for (int y = 10; y < 16; y++) mvhline(y, 40, ' ', 40);
+
+    	mvaddch(9, 39, ACS_ULCORNER);
+	    mvaddch(9, 80, ACS_URCORNER);
+	    mvaddch(16, 39, ACS_LLCORNER);
+    	mvaddch(16, 80, ACS_LRCORNER);
+    	mvhline(9, 40, ACS_HLINE, 40);
+	    mvhline(16, 40, ACS_HLINE, 40);
+	    mvvline(10, 39, ACS_VLINE, 6);
+    	mvvline(10, 80, ACS_VLINE, 6);
+
+	    mvprintw(12, 55, "SIN EDUCACIÓN NO HAY DEMOCRACIA");
+
+      for (int y = 10; y < 16; y++) mvhline(y, 40, ' ', 40);
+
+    	mvaddch(9, 39, ACS_ULCORNER);
+	    mvaddch(9, 80, ACS_URCORNER);
+	    mvaddch(16, 39, ACS_LLCORNER);
+    	mvaddch(16, 80, ACS_LRCORNER);
+    	mvhline(9, 40, ACS_HLINE, 40);
+	    mvhline(16, 40, ACS_HLINE, 40);
+	    mvvline(10, 39, ACS_VLINE, 6);
+    	mvvline(10, 80, ACS_VLINE, 6);
+
+	    mvprintw(12, 55, "MILEI VETAME ESTA");
+
+    
     // disminuimos las vidas del estudiante.
     rEstudiante.setVidas(rEstudiante.getVidas() - 1);
-
     // volvemos a colocar al manifestante al costado izq
     m_y = rand() % 38 + 1; 
     m_x = 1;
